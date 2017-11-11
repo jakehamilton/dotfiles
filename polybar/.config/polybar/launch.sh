d8f7cd9f6551c9707859a5b8d4ec46d9f6ee255a
Jake Hamilton <jehamilton122@gmail.com>
2017-11-10 23:13:36 -0800

    [Feature] Cleanup polybar and play around

--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -6,8 +6,7 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-# Launch bar1 and bar2
-polybar short &
-
+# Launch bar
+polybar full &
 
 echo "Bars launched..."
